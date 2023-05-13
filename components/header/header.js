8d7d45cb5b768f4573b56f825df4659da7e93817
Matteo  Marengo <matteo.marengo@excentio.com>
2023-05-13 19:02:55 +0200

    header html header image

---
+++ components/header/header.js
@@ -0,0 +1,18 @@
+Vue.component("header-home", {
+  template: `
+    <header>
+        <navbar></navbar>
+        <div class="home-text-image">
+            <div class="text-box">
+                <p class="home-title">Trova l'insegnante ideale</p>
+                <p class="home-subtitle">Lezioni online o in presenza!</p>
+            </div>
+            <img src="./assets/home-image.png" class="home-image"/>
+        </div>
+    </header> 
+  `,
+
+  data() {
+    return {};
+  },
+});
