ef1e4366809c0c6eb0899673034ac9c04823f46a
Matteo  Marengo <matteo.marengo@excentio.com>
2023-05-13 19:03:25 +0200

    navbar css componente navbar per vue

---
+++ components/navbar/navbar.js
@@ -0,0 +1,20 @@
+Vue.component("navbar", {
+  template: `
+    <nav>
+        <div>
+            <a href="#home" class="primary-text">Home</a>
+            <a href="#" class="primary-text">?</a>
+            <a href="#" class="primary-text">?</a>
+            <a href="#" class="primary-text">?</a>
+        </div>
+        <div>
+            <button class="primary-button">Accedi</button>
+            <button class="primary-button">Iscriviti</button>
+        </div>
+    </nav>
+  `,
+
+  data() {
+    return {};
+  },
+});
