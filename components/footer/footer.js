523dc21209e21a00274a7c244f3d16d98a65c475
Matteo  Marengo <matteo.marengo@excentio.com>
2023-05-16 23:13:17 +0200

    footer

---
+++ components/footer/footer.js
@@ -0,0 +1,17 @@
+Vue.component("footer-component", {
+  template: `
+    <footer>
+        <div class="download-app">
+            <p>Scarica l'app ufficiale</p>
+            <div>
+              <img src="assets/apple.svg" class="footer-icon-size"/>
+              <img src="assets/google-play.svg" class="footer-icon-size"/>
+            </div> 
+        </div> 
+    </footer> 
+  `,
+
+  data() {
+    return {};
+  },
+});
