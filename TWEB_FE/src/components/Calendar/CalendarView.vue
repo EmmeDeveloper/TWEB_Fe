2a5ca28afb55786547da95e2cf200b004bf3713d
Matteo  Marengo <matteo.marengo@excentio.com>
2023-08-03 10:12:31 +0200

    fix ora

--- TWEB_FE/src/components/Calendar/CalendarView.vue
+++ TWEB_FE/src/components/Calendar/CalendarView.vue
@@ -1,73 +1,87 @@
-
 <script setup>
-import { ref, computed } from 'vue';
-import { getCoursesRepetitions, getUserRepetitions, useStore } from '../../StateService.js';
-import LessonCalendarView from '../LessonCalendar/LessonCalendarView.vue';
-import FutureLessonReservationView from '../LessonReservation/FutureLessonReservationView.vue';
-import PastLessonReservationView from '../LessonReservation/PastLessonReservationView.vue';
-
+import { ref, computed } from 'vue'
+import { getCoursesRepetitions, getUserRepetitions, useStore } from '../../StateService.js'
+import LessonCalendarView from '@/components/LessonCalendar/LessonCalendarView.vue'
+import FutureLessonReservationView from '@/components/LessonReservation/FutureLessonReservationView.vue'
+import PastLessonReservationView from '@/components/LessonReservation/PastLessonReservationView.vue'
 
 const prof = {
-  '1': [{ id: '1', name: 'Giovanni', surname: 'Bianchi' }, { id: '2', name: 'Mario', surname: 'Rossi' }],
-  '2': [{ id: '3', name: 'Luca', surname: 'Verdi' }, { id: '4', name: 'Giuseppe', surname: 'Neri' }],
-  '3': [{ id: '5', name: 'Giacomo', surname: 'Gialli' }]
-};
-const state = ref(useStore());
+  1: [
+    { id: '1', name: 'Giovanni', surname: 'Bianchi' },
+    { id: '2', name: 'Mario', surname: 'Rossi' }
+  ],
+  2: [
+    { id: '3', name: 'Luca', surname: 'Verdi' },
+    { id: '4', name: 'Giuseppe', surname: 'Neri' }
+  ],
+  3: [{ id: '5', name: 'Giacomo', surname: 'Gialli' }]
+}
+const state = ref(useStore())
 
 const lessonsMap = computed(() => {
-  const map = {};
+  const map = {}
   state.value.allRepetitions.forEach((repetition) => {
-    const date = new Date(repetition.date);
-    const key = date.toISOString().slice(0, 10); // Using toISOString() to get "YYYY-MM-DD" format
+    const date = new Date(repetition.date)
+    const key = date.toISOString().slice(0, 10) // Using toISOString() to get "YYYY-MM-DD" format
     if (!map[key]) {
-      map[key] = [];
+      map[key] = []
     }
-    map[key].push(repetition);
-  });
-  return map;
-});
+    map[key].push(repetition)
+  })
+  return map
+})
 
 function repetitionUpdated() {
-  selectedItem.value = null;
-  getCoursesRepetitions();
-  getUserRepetitions();
+  selectedItem.value = null
+  getCoursesRepetitions()
+  getUserRepetitions()
 }
 
-
-const selectedItem = ref(null);
+const selectedItem = ref(null)
 
 function selectItem(item) {
-  selectedItem.value = item;
+  selectedItem.value = item
 }
-
 </script>
 
 <template>
-<div>
-
   <div>
-    <div v-for="repetition in state.userRepetitions" :key="repetition.id">
-      ----------------------------------
-      <div>{{ repetition.date }}</div>
-      <div>{{ repetition.time }}</div>
-      <div>{{ repetition.status }}</div>
-      <div>{{ repetition.note }}</div>
-      <div>{{ repetition.professor }}</div>
-      <div>{{ repetition.user }}</div>
-      <div>{{ repetition.course }}</div>
-      <div>{{ repetition.id }}</div>
+    <div>
+      <div v-for="repetition in state.userRepetitions" :key="repetition.id">
+        ----------------------------------
+        <div>{{ repetition.date }}</div>
+        <div>{{ repetition.time }}</div>
+        <div>{{ repetition.status }}</div>
+        <div>{{ repetition.note }}</div>
+        <div>{{ repetition.professor }}</div>
+        <div>{{ repetition.user }}</div>
+        <div>{{ repetition.course }}</div>
+        <div>{{ repetition.id }}</div>
+      </div>
+    </div>
+
+    <LessonCalendarView
+      :lessonsMap="lessonsMap"
+      @repetitionUpdated="repetitionUpdated"
+      @selectItem="selectItem"
+    />
+
+    <div>
+      <FutureLessonReservationView
+        v-if="selectedItem?.showFuture"
+        :repetition="selectedItem.repetition"
+        :time="selectedItem.time"
+        :date="selectedItem.date"
+        :courseProfMap="prof"
+        @reservedLesson="repetitionUpdated"
+        @deletedLesson="repetitionUpdated"
+      />
+
+      <PastLessonReservationView
+        v-else-if="selectedItem?.showPast"
+        :repetition="selectedItem.repetition"
+        @updatedLesson="repetitionUpdated"
+      />
     </div>
   </div>
-
-  <LessonCalendarView :lessonsMap="lessonsMap" @repetitionUpdated="repetitionUpdated" @selectItem="selectItem" />
-
-  <div>
-      <FutureLessonReservationView v-if="selectedItem?.showFuture" :repetition="selectedItem.repetition"
-        :time="selectedItem.time" :date="selectedItem.date" :courseProfMap="prof" @reservedLesson="repetitionUpdated"
-        @deletedLesson="repetitionUpdated" />
-
-      <PastLessonReservationView v-else-if="selectedItem?.showPast" :repetition="selectedItem.repetition"
-        @updatedLesson="repetitionUpdated" />
-    </div>
-</div>
 </template>
--- TWEB_FE/src/components/LessonReservation/LessonReservationDetails.vue
+++ TWEB_FE/src/components/LessonReservation/LessonReservationDetails.vue
@@ -1,36 +1,33 @@
 <template>
   {{ props }}
 
-
   <div :style="{ width: '100%', padding: '0 16px' }">
-    <h2 v-if="props.title" style="font-weight: bold; font-size: 20px; padding: 16px 0;">
+    <h2 v-if="props.title" style="font-weight: bold; font-size: 20px; padding: 16px 0">
       {{ props.title }}
     </h2>
 
-    <div style="display: flex; align-items: center; padding-top: 8px;">
-      <i class="mdi mdi-calendar-blank" style="padding-right: 8px;"></i>
-      <span style="font-size: 16px;">
+    <div style="display: flex; align-items: center; padding-top: 8px">
+      <i class="mdi mdi-calendar-blank" style="padding-right: 8px"></i>
+      <span style="font-size: 16px">
         {{ formatDate(props.date) }}
       </span>
     </div>
 
-    <div style="display: flex; align-items: center; padding-top: 8px;">
-      <i class="mdi mdi-clock-time-five-outline" style="padding-right: 8px;"></i>
-      <span style="font-size: 16px;">
-        {{ formatTime(props.time) }}
-      </span>
+    <div style="display: flex; align-items: center; padding-top: 8px">
+      <i class="mdi mdi-clock-time-five-outline" style="padding-right: 8px"></i>
+      <span style="font-size: 16px"> {{ props.time }}:00 - {{ parseInt(time) + 1 }}:00 </span>
     </div>
 
-    <div v-if="props.course" style="display: flex; align-items: center; padding-top: 8px;">
-      <i class="mdi mdi-clipboard-list" style="padding-right: 8px;"></i>
-      <span style="font-size: 16px;">
+    <div v-if="props.course" style="display: flex; align-items: center; padding-top: 8px">
+      <i class="mdi mdi-clipboard-list" style="padding-right: 8px"></i>
+      <span style="font-size: 16px">
         {{ props.course.title }}
       </span>
     </div>
 
-    <div v-if="props.professor" style="display: flex; align-items: center; padding-top: 8px;">
-      <i class="mdi mdi-account-multiple" style="padding-right: 8px;"></i>
-      <span style="font-size: 16px;">
+    <div v-if="props.professor" style="display: flex; align-items: center; padding-top: 8px">
+      <i class="mdi mdi-account-multiple" style="padding-right: 8px"></i>
+      <span style="font-size: 16px">
         {{ props.professor.name }} {{ props.professor.surname }}
       </span>
     </div>
@@ -43,15 +40,12 @@ const props = defineProps({
   time: Number,
   date: Date,
   course: String,
-  professor: Object,
-});
+  professor: Object
+})
 
 function formatDate(date) {
-  const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
-  const formattedDate = date.toLocaleDateString('it-IT', options);
-  return formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1);
-};
-function formatTime(time) {
-  return `${time}:00 - ${time + 1}:00`;
-};
+  const options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' }
+  const formattedDate = date.toLocaleDateString('it-IT', options)
+  return formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1)
+}
 </script>
